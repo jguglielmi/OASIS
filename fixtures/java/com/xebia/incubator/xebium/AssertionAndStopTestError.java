da05a8d0d035e13dbe5f28d7b8fac3db88e46e28
Edward Jakubowski <ejakubowski7@gmail.com>
2014-06-15 09:50:55 -0400

    removed fixtures directory so maven and ant will share same src directory
    removed fixtures directory.
    Updated OasisPlugin.jar
    Added modified Fitnesse-standalone.jar that allows replacing ScriptTable
    to the custom Oasis Script Table.

--- fixtures/java/com/xebia/incubator/xebium/AssertionAndStopTestError.java
+++
@@ -1,9 +0,0 @@
-package com.xebia.incubator.xebium;
-
-public class AssertionAndStopTestError extends AssertionError {
-
-	public AssertionAndStopTestError(Object detailMessage) {
-		super(detailMessage);
-	}
-
-}
