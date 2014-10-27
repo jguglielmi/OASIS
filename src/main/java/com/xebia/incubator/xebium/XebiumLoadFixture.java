ea447d10cde260c779f4253a4ee479cce609e8a5
Edward Jakubowski <ejakubowski7@gmail.com>
2014-10-27 18:44:03 -0400

    Updated Selenium to version 2.44.0
    Had to remove JmeterDriverFixture and update some references.

--- src/main/java/com/xebia/incubator/xebium/XebiumLoadFixture.java
+++ src/main/java/com/xebia/incubator/xebium/XebiumLoadFixture.java
@@ -6,7 +6,8 @@ import com.thoughtworks.selenium.CommandProcessor;
 import com.thoughtworks.selenium.HttpCommandProcessor;
 import com.thoughtworks.selenium.SeleniumException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverCommandProcessor;
+//import org.openqa.selenium.WebDriverCommandProcessor;
+import com.thoughtworks.selenium.webdriven.WebDriverCommandProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
